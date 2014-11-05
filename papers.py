46ca5eef9cedf6f0517e340144925a01fa35d741
eugenekang <eugene.yc.kang@gmail.com>
2014-11-05 15:29:54 -0500

    Added selection for visit visa and transit visa Some selection for the Watchlist

--- papers.py
+++ papers.py
@@ -28,9 +28,15 @@ def decide(input_file, watchlist_file, countries_file):
     """
 
     # open files, convert files to python style
+    # example_entries.json
     with open(input_file, "r") as input_reader:
+        #File to string
         input_contents = input_reader.read()
+        # make string contents all uppercase
+        input_contents.lower()
+        #string load to dict/list, each entry in list is 1 record in the json
         input_contents = json.loads(input_contents)
+
     with open(watchlist_file, "r") as watchlist_reader:
         watchlist_contents = watchlist_reader.read()
         watchlist_contents = json.loads(watchlist_contents)
@@ -44,10 +50,12 @@ def decide(input_file, watchlist_file, countries_file):
     # create medical list
     medical_list = []
     # collect country code
+    #dictionary values()
     for line in countries_contents.values():
         if line["medical_advisory"] != "":
             medical_list.append(line["code"])
     # check if the traveller comes from the country that has a medical advisory
+    #entry is a genetic/var
     for entry in input_contents:
         if entry["from"]["country"] in medical_list:
             return["Quarantine"]
@@ -67,9 +75,8 @@ def decide(input_file, watchlist_file, countries_file):
             passport_list.append(line["passport"])
     # check traveller info vs watchlist
     for entry in input_contents:
-
-
-
+        if entry["passport"] in passport_list or entry["first_name"] in first_list or entry["last_name"] in last_list:
+            return["Secondary"]
 
     # check incomplete entry
     for entry in input_contents:
@@ -82,6 +89,41 @@ def decide(input_file, watchlist_file, countries_file):
         for froms in entry["from"].values():
             if froms == "":
                 return["Reject"]
+
+    # create lists for visit visas
+    visit_visa_list = []
+    for line in countries_contents.values():
+        if line["visitor_visa_required"] == 1:
+            visit_visa_list.append.lower(line["code"])
+
+    # create lists for transit visas
+    transit_visa_list = []
+    for line in countries_contents.values():
+        if line["transit_visa_required"] == 1:
+            transit_visa_list.append.lower(line["code"])
+
+    # check if visiting
+    for entry in input_contents:
+        if entry["entry_reason"] == "visit":
+            # if country is on visa requirement list
+            if entry["home"]["country"] in visit_visa_list:
+                try:
+                    # check for valid visa date
+                    if valid_visa_date(entry["visa"]["date"]) != True:
+                        return["Reject"]
+                except:
+                    return ["Reject"]
+
+    # check if in transit
+    for entry in input_contents:
+        if entry["entry_reason"] == "transit":
+            if entry["home"]["country"] in transit_visa_list:
+                try:
+                    if valid_visa_date(entry["visa"]["date"]) != True:
+                        return["Reject"]
+                except:
+                    return ["Reject"]
+
     #if
     #    return["Reject"]
     #elif
@@ -116,4 +158,17 @@ def valid_date_format(date_string):
         datetime.datetime.strptime(date_string, '%Y-%m-%d')
         return True
     except ValueError:
+        return False
+
+def valid_visa_date(date):
+    """
+    Checks to ensure the visa has a valid date, ie. issue date is < 2 year from current date.
+    :param date: visa date being checked
+    :return: True if date is within 2 years, False otherwise
+    """
+
+    valid_by_date = datetime.datetime.now() - datetime.timedelta(days=2*365.25)
+    if valid_by_date.strftime("%Y-%m-%d") < date.strftime("%Y-%m-%d"):
+        return True
+    else:
         return False
